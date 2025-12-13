cc2bf7981c03ea30beeef28db983bd30f950b078
Oluwatise Ajayi <dr393462@gmail.com>
2025-12-13 12:56:00 +0100

    feat: Add users controller with endpoints for QR code generation and health record sharing.

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -4,13 +4,13 @@ import { AuthGuard } from '@nestjs/passport';
 import { UsersService } from './users.service';
 
 @ApiTags('Users')
-@ApiBearerAuth()
-@UseGuards(AuthGuard('jwt'))
 @Controller('users')
 export class UsersController {
     constructor(private usersService: UsersService) { }
 
     @Get('qr')
+    @ApiBearerAuth()
+    @UseGuards(AuthGuard('jwt'))
     @ApiOperation({ summary: 'Get a QR code (Data URI) containing my health records' })
     async getMyQrCode(@Request() req) {
         // Determine base URL (protocol + host)
